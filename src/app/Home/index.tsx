363ac3bd60d118785c02f8713194155954bf14a6
IsaqueTADS <isaqueofc18@gmail.com>
2025-10-08 04:02:16 -0300

    Componente input

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View, Image } from "react-native";
 import { styles } from "./styles";
+import { Button } from "@/components/Button";
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
+
+      <Button title="Adicionar"  />
     </View>
   );
 }
--- src/app/Home/styles.ts
+++ src/app/Home/styles.ts
@@ -4,7 +4,9 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 62,
+    padding: 24,
+    gap: 24,
   },
   logo: {
     height: 34,
