2f971dfe73647a6ca722cd8dbdf90e297e7610b5
IsaqueTADS <isaqueofc18@gmail.com>
2025-12-06 00:42:33 -0300

    content

--- src/app/Home/styles.ts
+++ src/app/Home/styles.ts
@@ -5,12 +5,24 @@ export const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     paddingTop: 62,
-    padding: 24,
     backgroundColor: "#D0D2D8",
-    gap: 24,
   },
   logo: {
     height: 34,
     width: 134,
+  },
+  form: {
+    width: "100%",
+    paddingHorizontal: 16,
+    gap: 7,
+    marginTop: 42,
+  },
+  content: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: "#FFF",
+    borderTopLeftRadius: 24,
+    borderTopRightRadius: 24,
+    marginTop: 24,
   },
 });
---
+++ src/components/Filter/styles.ts
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View, Image } from "react-native";
+import { styles } from "./styles";
+import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
+
+export function Home() {
+  return (
+   
+  );
+}
